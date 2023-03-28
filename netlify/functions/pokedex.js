aec6a74c6c25f6712effc5afe7940e1537af9157
dlfree-xzn-ir <120237240+dlfree-xzn-ir@users.noreply.github.com>
2023-03-28 15:43:25 +0330

    Update pokedex.js

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,17 +1,24 @@
 import fetch from 'node-fetch'
 
 export const handler = async (event, context) => {
-  const eventBody = JSON.parse(event.body)
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region
-
-  const response = await fetch(POKE_API)
-  const data = await response.json()
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      event: event,
-      url: event.path + "?" + new URLSearchParams(event.queryStringParameters),
-    })
-  }
+  let path=event.path
+  path.replace("/.netlify/functions/pokedex", "/")
+  let url = new URL(path);
+  
+  if (url.pathname.startsWith('/')) {
+    url.hostname = 'frosty-glitter-b25e.spreadsheet22.workers.dev'
+    let new_request = new Request(url, event);
+    
+    //const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region
+    //const response = await fetch(POKE_API)
+    //const data = await response.json()
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        new_request: new_request,
+        url: event.path + "?" + new URLSearchParams(event.queryStringParameters),
+      })
+   }
+  
 }
